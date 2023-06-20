550ea85605d652aaf0cd5e955fec5a1d6fedf5fc
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-06-20 16:32:19 +0930

    Guns

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,20 +5,53 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     Rigidbody rb;
+    bool despawning = false;
+    [SerializeField] float despawnDelay = 0.1f;
+    float timer;
+    Collider col;
+    MeshRenderer mr;
+    TrailRenderer tr;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        mr = GetComponent<MeshRenderer>();
+        tr = GetComponent<TrailRenderer>();
+    }
+    private void Update()
+    {
+        if (despawning)
+        {
+            timer += Time.deltaTime;
+            if(timer > despawnDelay)
+            {
+                tr.time = 0;
+                GetComponent<PooledObj>().Despawn();
+            }
+        }
         
     }
 
     public void Shoot(Vector3 vel,Vector3 origin)
     {
-        rb = GetComponent<Rigidbody>();
+        
         rb.velocity = vel;
         transform.position = origin;
+        despawning = false;
+        col.enabled = true;
+        mr.enabled = true;
+        rb.isKinematic = false;
+        tr.time = .1f;
     }
+    
     private void OnCollisionEnter(Collision collision)
     {
-        GetComponent<PooledObj>().Despawn();
+        Debug.Log("Hit" + collision.collider.gameObject.name, collision.collider.gameObject);
+        
+        despawning=true;
+        col.enabled = false;
+        mr.enabled = false;
+        rb.isKinematic = true;
     }
 }
