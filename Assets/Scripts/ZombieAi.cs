84304a513b7291853067382f69776b9c251d1226
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-06-26 10:53:10 +0930

    ai

--- Assets/Scripts/ZombieAi.cs
+++ Assets/Scripts/ZombieAi.cs
@@ -5,15 +5,38 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class ZombieAi : MonoBehaviour
 {
+    NavMeshAgent agent;
+    [SerializeField] Transform target;
+    [SerializeField] float attackDistance = 1;
+    Animator animator;
+    [SerializeField] string attackTrigger = "Attack";
+    [SerializeField] string reset = "Reset";
+    Health health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        health = GetComponent<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
+        if(agent.remainingDistance < attackDistance)
+        {
+            animator.SetTrigger(attackTrigger);
+        }
+    }
+    public void Reset()
+    {
+        animator.SetTrigger(reset);
+        health.health = health.maxHealth;
+
+    }
+    public void DealDamage()
+    {
+
     }
 }
