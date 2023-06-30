47d3a2feacbf6688ac52da49b0a56ab2364a3986
EUANS-PC\Euan Jones <s213321@students.aie.edu.au>
2023-06-30 11:01:09 +0930

    Prefab rearrangment, Added score canvas/ chalkboard asset into Prefabs>UI

---
+++ Assets/ScoreUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI wavesCount;
+    [SerializeField] TextMeshProUGUI killsCount;
+}
